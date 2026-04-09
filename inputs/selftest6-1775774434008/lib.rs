9b3193f999b4919619e44bd02dea3befecd2820d
sarvadigital2023-cmyk <sarvadigital2023@gmail.com>
2026-04-10 01:40:34 +0300

    Test lib.rs selftest6-1775774434008

---
+++ inputs/selftest6-1775774434008/lib.rs
@@ -0,0 +1,24 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod test_program {
+    use super::*;
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+    pub fn set_value(ctx: Context<SetValue>, val: u64) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetValue {}
