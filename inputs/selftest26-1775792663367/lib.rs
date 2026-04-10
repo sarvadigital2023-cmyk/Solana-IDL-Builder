e52c7030a75e98fdeea2a1aec4a3cade055a5e6a
sarvadigital2023-cmyk <sarvadigital2023@gmail.com>
2026-04-10 06:44:23 +0300

    selftest26 input

---
+++ inputs/selftest26-1775792663367/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod test_program {
+    use super::*;
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
